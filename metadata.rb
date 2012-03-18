e3349f62ce881c2f73f1e2f29cfb9511fc6e75a7
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-18 00:19:21 -0700

    chef versions do not support .pre suffixes

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ description       "Common recipe for EY Cloud"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer        "Dr Nic Williams"
 maintainer_email  "drnicwilliams@gmail.com"
-version           "0.2.0.pre"
+version           "0.2.0"
 # depends           "cronjobs"
