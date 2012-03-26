7a03625547f58c4f85d3c189d42247edb7049f09
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-26 08:42:40 -0700

    update_file supports replace

--- definitions/update_file.rb
+++ definitions/update_file.rb
@@ -1,5 +1,13 @@
 # Usage examples:
 #
+# Also can append if file exists, else create it.
+# update_file "add-#{name}-to-sysctl" do
+#   action :append
+#   path '/etc/sysctl.conf'
+#   body "#{name} = #{value}"
+# end
+#
+# Can also overwrite existing file if it exists.
 # update_file "/tmp/cron_update_header" do
 #   action :rewrite
 # 
@@ -10,18 +18,20 @@
 # CRON
 # end
 #
+# Can replace a selection of existing file:
+# update_file "/data/myapp/current/Gemfile" do
+#   action :replace
+#   where /(\s+)gem ['"]somegem['"].*$/
+#   body "$1gem 'somegem', :git => 'git://github.com/engineyard/somegem.git'"
+# end
+#
+# Default action is to :append. If `where` is specified, the defaults to :replace
 # update_file "local portage package.use" do
 #   path "/etc/portage/package.use/local"
 #   body full_name
 #   not_if "grep '#{full_name}' /etc/portage/package.use/local"
 # end
 #
-# Also can append:
-# update_file "add-#{name}-to-sysctl" do
-#   action :append
-#   path '/etc/sysctl.conf'
-#   body "#{name} = #{value}"
-# end
 define :update_file, :action => :append do
 
   filepath = params[:path] || params[:name]
@@ -37,6 +47,8 @@ define :update_file, :action => :append do
     only_if params[:only_if] if params[:only_if]
   end
 
+  params[:action] = :replace if params[:where]
+  
   case params[:action].to_sym
   when :append, :rewrite
 
@@ -45,6 +57,14 @@ define :update_file, :action => :append do
     execute "echo '#{params[:body]}' #{mode} #{filepath}" do
       not_if params[:not_if] if params[:not_if]
       only_if params[:only_if] if params[:only_if]
+    end
+
+  when :replace
+
+    ruby_block do
+      content = File.binread(params[:path])
+      content.gsub!(params[:where], params[:body])
+      File.open(path, 'wb') { |file| file.write(content) }
     end
 
   when :truncate
